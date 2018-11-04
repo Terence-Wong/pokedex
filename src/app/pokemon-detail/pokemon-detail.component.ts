4db55ccda81d86ddea44579b0b7d71e1ab87a271
terence <terencewong.work@gmail.com>
2018-11-04 01:18:58 -0400

    added types

--- src/app/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pokemon-detail/pokemon-detail.component.ts
@@ -32,7 +32,6 @@ export class PokemonDetailComponent implements OnInit {
 
   ngOnInit() {
     this.getPokemon();
-    
   }
   getPokemon(): void{
     const id = +this.route.snapshot.paramMap.get('id');
@@ -55,7 +54,59 @@ export class PokemonDetailComponent implements OnInit {
     }
     return '../../assets/images/250px-'+id+this.pokemon.name+'.png';
   }
-
-
   
+
+
+  //types
+  isFire(type: string): boolean{
+    return type === "Fire";
+  }
+  isNormal(type: string): boolean{
+    return type === "Normal";
+  }
+  isWater(type: string): boolean{
+    return type === "Water";
+  }
+  isElectric(type: string): boolean{
+    return type === "Electric";
+  }
+  isGrass(type: string): boolean{
+    return type === "Grass";
+  }
+  isIce(type: string): boolean{
+    return type === "Ice";
+  }
+  isFighting(type: string): boolean{
+    return type === "Fighting";
+  }
+  isPoison(type: string): boolean{
+    return type === "Poison";
+  }
+  isGround(type: string): boolean{
+    return type === "Ground";
+  }
+  isFlying(type: string): boolean{
+    return type === "Flying";
+  }
+  isPsychic(type: string): boolean{
+    return type === "Psychic";
+  }
+  isBug(type: string): boolean{
+    return type === "Bug";
+  }
+  isRock(type: string): boolean{
+    return type === "Rock";
+  }
+  isGhost(type: string): boolean{
+    return type === "Ghost";
+  }
+  isDragon(type: string): boolean{
+    return type === "Dragon"
+  }
+  isDark(type: string): boolean{
+    return type === "Dark"
+  }
+  isSteel(type: string): boolean{
+    return type === "Steel"
+  }
 }
